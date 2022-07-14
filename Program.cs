a167234f6a88ccc8995e30632380bfb86e272a09
Fırat Şakar <frtskr0@gmail.com>
2022-07-14 10:30:53 +0300

    done try-catch

--- Program.cs
+++ Program.cs
@@ -13,15 +13,35 @@
 //     Console.WriteLine("Hata: "+ ex.ToString());
 // }
 
+// try{
+//     var deneme = "test";
+//     int deneme2 = Convert.ToInt32(deneme);
+// }
+// //Hataya göre özelleştirme gerçekleştirdik. FormaatException yakalanırsa buraya girecektir. 
+// catch(FormatException ex){
+//      Console.WriteLine("Format Exception: "+ ex.ToString());
+// }
+// //Diğer tüm hatalar için bu bloğu kullanacak.
+// catch(Exception ex){
+//     Console.WriteLine("Hata: "+ ex.ToString());
+// }
+
 try{
     var deneme = "test";
     int deneme2 = Convert.ToInt32(deneme);
 }
-//Hataya göre özelleştirme gerçekleştirdik. FormaatException yakalanırsa buraya girecektir. 
-catch(FormatException ex){
-     Console.WriteLine("Format Exception: "+ ex.ToString());
+//Hata yönetimini farklı bir metof içerisinde gerçekleştirdik. Böylelikle temiz kod anlayışına uyarız.
+catch(FormatException ex) when (LogException(ex)){}
+catch(Exception ex) when (LogException(ex)){}
+
+//Her ihtimalde bu bloğa girer. Bitiş anında bir işlem yapılmak isteniyorsa burada gerçekleştirebiiliriz.
+finally {
+    Console.WriteLine("İşlem Bitti.");
 }
-//Diğer tüm hatalar için bu bloğu kullanacak.
-catch(Exception ex){
-    Console.WriteLine("Hata: "+ ex.ToString());
+
+
+static bool LogException(Exception ex){
+    return false;
 }
+
+
